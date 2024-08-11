8d3629be8639cbbaf4227367f84c9fcfaac33300
bert.destoop <bert.destoop@student.kuleuven.be>
2024-08-12 01:25:39 +0200

    get sound and calculate volume in db, max vol, min vol, relative volume (based on max, min and current volume), and time averaged relative volume.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import copy
 import sys
+import threading
+import time
 from tkinter import Tk
 
 import cv2 as cv
@@ -7,10 +9,12 @@ import numpy as np
 import pygame
 from PIL import Image, ImageFilter
 
+import sounddevice as sd
+import wavio as wv
+
 pygame.init()
 
-thread_settings = None
-
+# PYGAME
 HEIGHT = pygame.display.Info().current_h
 WIDTH = pygame.display.Info().current_w
 
@@ -23,7 +27,7 @@ pygame.display.set_caption('denoiser')
 FPS = 15
 clk = pygame.time.Clock()
 
-CODE_IMG_PATH = "images/test2_inv.png"
+CODE_IMG_PATH = "static/test2_inv.png"
 FORMAT = "RGB"
 img_orig = Image.open(CODE_IMG_PATH).convert(FORMAT)
 img_curr = img_orig.filter(ImageFilter.GaussianBlur(2))
@@ -32,14 +36,27 @@ invisibility = 0
 wdw_width = wdw.get_width()
 wdw_height = wdw.get_height()
 
+# SOUND
+volume = 0
+volume_max = 0
+volume_min = 1000
+volume_rel = 0
+volume_rel_avg = 0
+
+# THREADS
+running = True
+threads = []
+
 
 def handle_events():
-    global wdw, thread_settings
+    global wdw, thread_settings, threads, running
     for event in pygame.event.get():
         match event.type:
             case pygame.QUIT:
                 pygame.quit()
-
+                running = False
+                for t in threads:
+                    t.join()
                 sys.exit()
             case pygame.VIDEORESIZE:
                 update_sizing()
@@ -105,7 +122,7 @@ def draw_noise():
 
 
 def dials_main():
-    global tk_root
+    global running, tk_root
     # create root window
     tk_root = Tk()
 
@@ -119,23 +136,89 @@ def dials_main():
     tk_root.mainloop()
 
 
+def sound_main():
+    global running, volume, volume_max, volume_min, volume_rel, volume_rel_avg
+    sample_freq = 44100
+    rec_duration = 0.3  # seconds
+    run_avg_duration = 6  # seconds
+    vol_arr = [0] * int(run_avg_duration // rec_duration)
+    vol_arr_i = 0
+    while running:
+        # record
+        recording = sd.rec(int(rec_duration * sample_freq), channels=1, dtype='int16')
+        sd.wait()
+        volume = 20 * np.log10(np.absolute(np.average(recording)))  # to dB
+
+        # update metrics
+        if volume > volume_max:
+            volume_max = volume
+        if volume < volume_min:
+            volume_min = volume
+        volume_rel = (volume + abs(volume_min)) / (volume_max + abs(volume_min))
+
+        vol_arr[vol_arr_i] = volume_rel
+        volume_rel_avg = np.average(vol_arr)
+
+        vol_arr_i += 1
+        vol_arr_i = vol_arr_i % len(vol_arr)
+
+        print(
+            f"vrel={volume_rel:.2f}, vrela={volume_rel_avg:.2f}, v={volume:.2f}, vmax={volume_max:.2f}, vmin={volume_min:.2f}")
+
+
+def draw_volumes():
+    font = pygame.font.Font('static/Uroob-Regular.ttf', 50)
+
+    circle_x = wdw_width // 6
+    circle_y = wdw_height - (wdw_height * volume_rel)
+    pygame.draw.circle(wdw, (0, 255, 255), (circle_x, circle_y), 10)
+    text = font.render('vrel', True, (100, 255, 100))
+    wdw.blit(text, text.get_rect(center=(circle_x, wdw_height - 10)))
+
+    circle_x = 2 * wdw_width // 6
+    circle_y = wdw_height - (wdw_height * volume_rel_avg)
+    pygame.draw.circle(wdw, (0, 255, 255), (circle_x, circle_y), 10)
+    text = font.render('vrela', True, (100, 255, 100))
+    wdw.blit(text, text.get_rect(center=(circle_x, wdw_height - 10)))
+
+    circle_x = 3 * wdw_width // 6
+    circle_y = wdw_height // 2 - volume
+    pygame.draw.circle(wdw, (0, 255, 255), (circle_x, circle_y), 10)
+    text = font.render('vol', True, (100, 255, 100))
+    wdw.blit(text, text.get_rect(center=(circle_x, wdw_height - 10)))
+
+    circle_x = 4 * wdw_width // 6
+    circle_y = wdw_height // 2 - volume_max
+    pygame.draw.circle(wdw, (0, 255, 255), (circle_x, circle_y), 10)
+    text = font.render('vmax', True, (100, 255, 100))
+    wdw.blit(text, text.get_rect(center=(circle_x, wdw_height - 10)))
+
+    circle_x = 5 * wdw_width // 6
+    circle_y = wdw_height // 2 - volume_min
+    pygame.draw.circle(wdw, (0, 255, 255), (circle_x, circle_y), 10)
+    text = font.render('vmin', True, (100, 255, 100))
+    wdw.blit(text, text.get_rect(center=(circle_x, wdw_height - 10)))
+
+
 if __name__ == '__main__':
     # thread_settings = threading.Thread(target=dials_main)
     # thread_settings.start()
+
+    thread_sound = threading.Thread(target=sound_main)
+    thread_sound.start()
 
     while True:
         handle_events()
 
         wdw.fill((0, 0, 0))
 
-        draw_noise()
+        # draw_noise()
+        draw_volumes()
 
         pygame.display.update()
         pygame.display.flip()
 
         invisibility += 1
         invisibility = invisibility % 20
-
-        print(invisibility)
 
         clk.tick(FPS)
